37dfea22f057ba596173fe815494a8d5b3412305
Aditya Verma <adityav090304@gmail.com>
2024-09-03 01:25:32 +0530

    Add video url

--- client/src/components/VideoModal.jsx
+++ client/src/components/VideoModal.jsx
@@ -1,3 +1,74 @@
+// import * as React from "react";
+// import Backdrop from "@mui/material/Backdrop";
+// import Box from "@mui/material/Box";
+// import Modal from "@mui/material/Modal";
+// import Fade from "@mui/material/Fade";
+// import GoldenButton from "./GoldenButton";
+
+// const style = {
+//   position: "absolute",
+//   top: "50%",
+//   left: "50%",
+//   transform: "translate(-50%, -50%)",
+//   width: 900,
+//   height: 600,
+//   bgcolor: "background.paper",
+//   border: "2px solid #000",
+//   boxShadow: 24,
+//   p: 4,
+// };
+
+// export default function VideoModal() {
+//   const [open, setOpen] = React.useState(false);
+//   const handleOpen = () => setOpen(true);
+//   const handleClose = () => setOpen(false);
+
+//   return (
+//     <div>
+//       <GoldenButton onClick={handleOpen} className=" flex gap-4">
+//         <svg
+//           xmlns="http://www.w3.org/2000/svg"
+//           viewBox="0 0 24 24"
+//           fill="#999999"
+//           className="size-6"
+//         >
+//           <path
+//             fillRule="evenodd"
+//             d="M4.5 5.653c0-1.427 1.529-2.33 2.779-1.643l11.54 6.347c1.295.712 1.295 2.573 0 3.286L7.28 19.99c-1.25.687-2.779-.217-2.779-1.643V5.653Z"
+//             clipRule="evenodd"
+//           />
+//         </svg>
+//         Play video
+//       </GoldenButton>
+//       <Modal
+//         aria-labelledby="transition-modal-title"
+//         aria-describedby="transition-modal-description"
+//         open={open}
+//         onClose={handleClose}
+//         closeAfterTransition
+//         slots={{ backdrop: Backdrop }}
+//         slotProps={{
+//           backdrop: {
+//             timeout: 500,
+//           },
+//         }}
+//       >
+//         <Fade in={open}>
+//           <Box sx={style}>
+//             <iframe
+//               className="w-full h-full"
+//               src="https://www.youtube.com/embed/C_UgSQ-nphY"
+//               title="YouTube video player"
+//               frameBorder="0"
+//               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+//               allowFullScreen
+//             ></iframe>
+//           </Box>
+//         </Fade>
+//       </Modal>
+//     </div>
+//   );
+// }
 import * as React from "react";
 import Backdrop from "@mui/material/Backdrop";
 import Box from "@mui/material/Box";
@@ -10,12 +81,19 @@ const style = {
   top: "50%",
   left: "50%",
   transform: "translate(-50%, -50%)",
-  width: 900,
-  height: 600,
+  width: {
+    xs: "90%", // 90% width on extra-small screens
+    sm: "80%", // 80% width on small screens
+    md: 700, // Fixed width on medium and larger screens
+  },
+  height: {
+    xs: "auto", // Auto height on extra-small screens
+    md: 500, // Fixed height on medium and larger screens
+  },
   bgcolor: "background.paper",
   border: "2px solid #000",
   boxShadow: 24,
-  p: 4,
+  p: 2, // Padding adjusted for smaller screens
 };
 
 export default function VideoModal() {
@@ -25,7 +103,7 @@ export default function VideoModal() {
 
   return (
     <div>
-      <GoldenButton onClick={handleOpen} className=" flex gap-4">
+      <GoldenButton onClick={handleOpen} className="flex gap-4">
         <svg
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 24 24"
@@ -55,13 +133,14 @@ export default function VideoModal() {
       >
         <Fade in={open}>
           <Box sx={style}>
-            <video className="w-full h-full object-cover" controls>
-              <source
-                src="https://www.youtube.com/watch?v=C_UgSQ-nphY"
-                type="video/mp4"
-              />
-              Your browser does not support the video tag.
-            </video>
+            <iframe
+              className="w-full h-full"
+              src="https://www.youtube.com/embed/C_UgSQ-nphY"
+              title="YouTube video player"
+              frameBorder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowFullScreen
+            ></iframe>
           </Box>
         </Fade>
       </Modal>
